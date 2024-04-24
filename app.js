968d8966a135b0c2c9321e65fbef31946be0792f
KimDayeong <dyyukim@gmail.com>
2024-04-24 14:02:43 +0900

    add weather icon

--- app.js
+++ app.js
@@ -7,13 +7,14 @@ const box1 = document.querySelector(".weather-box__main");
 const box2 = document.querySelector(".weather-box__sub1");
 const box3 = document.querySelector(".weather-box__sub2");
 
+const icon = document.getElementById("icon");
 const temp = document.getElementById("temp");
 const city = document.getElementById("city");
 const tempMin = document.getElementById("temp-min");
 const tempMax = document.getElementById("temp-max");
 const humidity = document.getElementById("humidity");
 const windSpeed = document.getElementById("wind-speed");
-const alert = document.getElementById("alert");
+const noCity = document.getElementById("noCity");
 
 submitBtn.addEventListener("click", onBtnClick);
 
@@ -27,6 +28,7 @@ function onBtnClick(event) {
 function createUrl(city) {
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
   getInfo(apiUrl);
+  //console.log(apiUrl);
 }
 
 function getInfo(url) {
@@ -42,11 +44,12 @@ function getInfo(url) {
 }
 
 function printInfo(data) {
-  alert.classList.add("hidden");
+  noCity.classList.add("hidden");
   box1.classList.remove("hidden");
   box2.classList.remove("hidden");
   box3.classList.remove("hidden");
 
+  icon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
   temp.innerText = `${data.main.temp}°`;
   city.innerText = data.name;
   tempMin.innerText = `${data.main.temp_min}°`;
@@ -56,7 +59,7 @@ function printInfo(data) {
 }
 
 function cityNotFound() {
-  alert.classList.remove("hidden");
+  noCity.classList.remove("hidden");
   box1.classList.add("hidden");
   box2.classList.add("hidden");
   box3.classList.add("hidden");
